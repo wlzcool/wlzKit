935475f05422ee46c7f974cf5384dc506cfb9f16
wlzcool <346594796@qq.com>
2015-08-31 22:06:48 +0800

    初次

---
+++ wlzKit/Controllers/TextHelperController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wlzKit.Controllers
+{
+    public class TextHelperController : Controller
+    {
+        //
+        // GET: /TextHelper/
+
+        public ActionResult EncodingGb2312()
+        {
+            return View();
+        }
+        public ActionResult 
+    }
+}
